fa5ceda3f3822864e49799a792a455d7d41ee361
yutakobayashidev <yutakobayashi0924@gmail.com>
2021-09-25 18:40:36 +0900

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,11 +15,8 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           type="image/png"
           href={`${siteConfig.siteRoot}/icon.png`}
         />
-        <link
-          rel="apple-touch-icon-precomposed"
-          type="image/png"
-          href={`${siteConfig.siteRoot}/icon.png`}
-        />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🪐</text></svg>" />
+        <link rel="icon alternate" type="image/png" href="https://twemoji.maxcdn.com/v/13.0.2/72x72/1f638.png" />
         <meta property="og:image" content={`${siteConfig.siteRoot}/icon.png`} />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary" />
