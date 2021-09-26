d0bb9dfa1c8cd6472bbd21a9ab7da2f8d429d370
yutakobayashidev <yutakobayashi0924@gmail.com>
2021-09-26 17:42:46 +0900

    Profile Update

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -5,11 +5,17 @@ export const HomeHero = () => {
   return (
     <div className={styles.container}>
       <div className={styles.emoji}>
-      <span>🪐</span>
+        <span>🪐</span>
       </div>
       <h1 className={styles.title}>Hi, I'm Yuta Kobayashi</h1>
       <p className={styles.description}>
-      My hobbies are programming, music, and space, and I enrolled in N middle school in April.
+        My hobbies are programming, music, and space, and I enrolled in N middle school in April.
+        You can follow{' '}
+        <a href="https://twitter.com/yutakobayashi">
+          @yutakobayashi
+          <TwitterIcon width={18} height={18} />
+        </a>{' '}
+        if you like.
       </p>
     </div>
   );
