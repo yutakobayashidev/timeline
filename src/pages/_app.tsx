ff9c1ee9b61af0f076490297f4fa88f16e83854c
yutakobayashidev <yutakobayashi0924@gmail.com>
2021-09-26 18:30:03 +0900

    og fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,16 +10,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link
-          rel="icon shortcut"
-          type="image/png"
-          href={`${siteConfig.siteRoot}/icon.png`}
-        />
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🪐</text></svg>" />
         <link rel="icon alternate" type="image/png" href="https://twemoji.maxcdn.com/v/13.0.2/72x72/1f638.png" />
-        <meta property="og:image" content={`${siteConfig.siteRoot}/icon.png`} />
+        <meta property="og:image" content={`${siteConfig.siteRoot}/ogp.png`} />
         <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Component {...pageProps} />
       <SiteFooter />
