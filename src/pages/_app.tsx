c37ff4fb15403a0a0ed11a2bcfac46f7a483b1d9
yutakobayashidev <yutakobayashi0924@gmail.com>
2021-09-28 02:58:16 +0900

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🪐</text></svg>" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🌏</text></svg>" />
         <link rel="icon alternate" type="image/png" href="https://twemoji.maxcdn.com/2/72x72/1f30f.png" />
         <meta property="og:image" content={`${siteConfig.siteRoot}/ogp.png`} />
         <meta property="og:type" content="website" />
